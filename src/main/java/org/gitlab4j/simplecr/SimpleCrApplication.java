0b6beadc4e7b30b49f5f58b18d93f0b0626dcd12
bw1832 <bw1832@gmail.com>
2025-04-18 16:11:32 +0900

    Update SimpleCrApplication.java

--- src/main/java/org/gitlab4j/simplecr/SimpleCrApplication.java
+++ src/main/java/org/gitlab4j/simplecr/SimpleCrApplication.java
@@ -20,6 +20,15 @@ public class SimpleCrApplication implements WebMvcConfigurer {
 		SpringApplication.run(SimpleCrApplication.class, args);
 	}
 
+
+
+
+
+
+
+
+
+	
     /**
      * To allow for branch names with slashes a UrlPathHelper must be created so that the path params
      * are not automatically decoded which breaks request path matching.
